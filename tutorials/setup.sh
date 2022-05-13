a5440a0afa5cfd6ae0ea9c85c93d800fdc99001a
Spencer Krum <nibz@spencerkrum.com>
2022-05-13 11:28:47 -0500

    Improve setup.sh user experience

--- tutorials/setup.sh
+++ tutorials/setup.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 set -eux
 
+# Get setup dir
+SETUP_DIR=$(dirname `readlink -f $0`)
+
+PYTHON=python
+# Compat for python3
+if ! command -v python &> /dev/null
+then
+    PYTHON=python3
+fi
+
 # Install pip requirements
 pip install -r requirements.txt
 
+
 # Get the bert model for the question answering demo
-python -m transformers.onnx --model=bert-large-uncased-whole-word-masking-finetuned-squad --feature=question-answering question_answering
+${PYTHON} -m transformers.onnx --model=bert-large-uncased-whole-word-masking-finetuned-squad --feature=question-answering qa
 
 # Get the gpt2 model for the generation model
-wget https://github.com/onnx/models/blob/main/text/machine_comprehension/gpt-2/model/gpt2-lm-head-10.onnx -O generation/gpt2-lm-head-10.onnx
-
+curl -fsSL -o generation/gpt2-lm-head-10.onnx https://github.com/onnx/models/blob/main/text/machine_comprehension/gpt-2/model/gpt2-lm-head-10.onnx
 # Install ariel lib
-pip install ../ariel
+pip install ${SETUP_DIR}/../ariel
 
 # Install aws cli if not already installed
 if ! command -v aws &> /dev/null
 then
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    rm -f awscliv2.zip
+    curl -fsSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    rm -fr aws
     unzip awscliv2.zip
     sudo ./aws/install
 fi
@@ -25,7 +37,7 @@ fi
 # Install kubectl if not already installed
 if ! command -v kubectl &> /dev/null
 then
-    curl -LO "https://dl.k8s.io/release/v1.22.0/bin/linux/amd64/kubectl"
+    curl -fsSL -O "https://dl.k8s.io/release/v1.22.0/bin/linux/amd64/kubectl"
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 fi
 
